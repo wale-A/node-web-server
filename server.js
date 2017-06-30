9b24696dc3a9a01fbe0efaf95c9e8d1874ff3b37
wale_A <isiaq.oa@gmail.com>
2017-06-30 03:57:25 +0100

    addition of projects page

--- server.js
+++ server.js
@@ -49,6 +49,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'projects page'
+    });
+});
+
  app.listen(port, () => {
      console.log(`server is up on port ${port}`);
  });
